7abc0d87738bb50baa442c2f53b4523afbf77672
ChrisWphoto <walter.chris@gmail.com>
2015-09-22 10:23:14 -0500

    added route to increment task

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,20 @@ router.get('/', function(req, res, next) {
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
+//get all users
 router.get('/users', function (req,res,next) {
   console.log('Getting all Users /users');
   User.find(function (err, users) {
     if(err) return next(err);
     res.json(users);
-    //console.log('get request users: ' + users);
+
   });
 });
 
 //posting new users
 router.post('/users', function (req,res,next) {
   var user = new User(req.body);
-  console.log('posting' + req.body.username);
+  console.log('posting /users ' + req.body.username);
 
   user.save(function (err,user) {
     if(err)return next(err);
@@ -29,21 +30,38 @@ router.post('/users', function (req,res,next) {
   });
 });
 
-//Has user been invited? Search for usernmae in friendName
-router.put('/register', function (req,res,next) {
-  console.log('register looking for: '+ req.body.username);
+//Check if user was invited and send back match
+//if no match save user name and email as new user
+//@needs work
+router.put('/register', function(req,res,next){
   User.findOne({friendName: req.body.username}, function (err, match) {
+    if (err) return next(err);
+    if (match) res.json(match);
+    else {
+      var user = new User(req.body);
+      user.save(function (err,user) {
+        if(err) return next(err);
+        res.json(user);
+      });
+    }
+  });
+});
+
+//Plus 1 to users complted task
+router.put('/taskCompleted', function (req,res,next) {
+  console.log('upBoating a Task for ' + req.body.username);
+  User.findOne({username: req.body.username}, function (err, match) {
     if(err){
-      console.log('error in findOne');
+      console.log('error in findOne upvote');
       return next(err);
     }
-    if(!match) {
-      console.log('no match found in register route');
-      return next(new Error("username does not exist in any friendName"));
-    }
-    console.log('sending something '+ match.username);
-    res.json(match);
-
+    if (match){
+    match.complete(function(err, user) {
+      console.log('calling complete: ' + user.taskCompleted);
+      if (err){return next(err);}
+      res.json(user);
+    });
+  } else {console.log('cannot upvote null')}
   });
 });
 
